cb15709b28fe0b17c497d3ada09d311cd44d9004
Tikhon Jelvis <tikhon@jelv.is>
2012-12-27 23:21:38 -0800

    Added a demo executable that just tries to synthesize a simple program. It doesn't work because of memory leaks.

---
+++ src/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import           Control.Monad.Random            (evalRandIO)
+
+import           Language.Forth.Distance         (registers)
+import           Language.Forth.Parse            ()
+import           Language.Forth.State            (State (..), startState)
+import           Language.Forth.Synthesis        (Program, defaultMutations,
+                                                  defaultOps, evaluate)
+
+import qualified Language.Synthesis.Distribution as Distr
+import           Language.Synthesis.Synthesis    (Problem (..), runningBest,
+                                                  synthesizeMhList)
+
+main :: IO ()
+main = evalRandIO (synthesizeMhList inclusiveOr) >>= print . (!! 100) . runningBest
+
+inclusiveOr :: Problem Program State
+inclusiveOr = Problem { score = evaluate program cases distance
+                      , prior = Distr.replicate 8 defaultOps
+                      , jump  = defaultMutations}
+  where program = read "over over or a! and a or"
+        cases = [startState {t = 0 }, startState {t = maxBound},
+                 startState {t = 1}, startState {t = maxBound - 1}]
+        distance = registers [t]
