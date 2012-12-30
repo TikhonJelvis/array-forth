2a2c0613fbaf1110befe0a08c8084637a74e5f0c
Tikhon Jelvis <tikhon@jelv.is>
2012-12-29 16:47:10 -0800

    Made the stack type a strict, unboxed vector internally.

--- src/Language/Forth/Stack.hs
+++ src/Language/Forth/Stack.hs
@@ -1,24 +1,31 @@
+{-# LANGUAGE BangPatterns #-}
 module Language.Forth.Stack (empty, push, pop, fill, Stack) where
+
+import           Prelude                     hiding ((++))
+
+import           Data.List                   (foldl')
+import           Data.Vector.Unboxed         ((!), (++))
+import qualified Data.Vector.Unboxed         as V
 
 import           Language.Forth.Instructions
 
-newtype Stack = Stack [F18Word] deriving (Eq)
+newtype Stack = Stack (V.Vector Int) deriving (Eq)
 
-instance Show Stack where show (Stack body) = unwords $ map show body
+instance Show Stack where show (Stack body) = unwords . map show $ V.toList body
 
 -- | A stack containing only 0s.
 empty :: Stack
-empty = Stack $ replicate 8 0
+empty = Stack $ V.replicate 8 0
 
 -- | Pushes the given element on top of the stack, discarding the last element.
 push :: Stack -> F18Word -> Stack
-push (Stack body) word = Stack $ word : init body
+push !(Stack body) word = Stack . V.cons (fromIntegral word) $ V.init body
 
 -- | Pops the top of the stack, returning the value and the new stack.
 pop :: Stack -> (Stack, F18Word)
-pop (Stack [])     = error "Empty stack."
-pop (Stack (x:xs)) = (Stack $ xs ++ [x], x)
+pop !(Stack body) = let x = V.take 1 body in (Stack $ V.tail body ++ x, fromIntegral $ x ! 0)
 
 -- | Push the given elements onto the stack one-by-one.
 fill ::  Stack -> [F18Word] -> Stack
-fill = foldl push
+fill = foldl' push
+
