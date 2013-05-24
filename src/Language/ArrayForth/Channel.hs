38dea3cf74dea812d7663305b1b0bbc90b850be0
Tikhon Jelvis <tikhon@jelv.is>
2013-05-23 23:06:39 -0700

    Extracted the channel stuff into its own file. Updated the rest of my code to use the new channel representation.

---
+++ src/Language/ArrayForth/Channel.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE RecordWildCards     #-}
+
+-- | Defines the basic operations for reading and writing through ports.
+--
+-- Each core has four ports connecting it to its neighbors. The cores
+-- around the edges have ports connected to IO devices. A "Channel" is
+-- just a type containing the four ports that you can write to or read
+-- from.
+module Language.ArrayForth.Channel where
+
+import           Control.Applicative        ((<|>))
+
+import           Data.Bits                  (testBit)
+import           Data.Monoid                (Monoid (..))
+
+import           Language.ArrayForth.Opcode (F18Word)
+
+-- | A channel representing the four communication directions a core
+-- may use. In practice, these will either be hooked up to other cores
+-- or to IO. Nothing represents no message; if there is a word,
+-- execution will block.
+data Channel = Channel { right, down, left, up :: Maybe F18Word } deriving (Show, Eq)
+
+-- The monoid instance is based around *replacement*.
+instance Monoid Channel where
+  mempty = emptyChannel
+  c₁ `mappend` c₂ = Channel { right = right c₁ <|> right c₂
+                            , down  = down c₁  <|> down c₂
+                            , left  = left c₁  <|> left c₂
+                            , up    = up c₁    <|> up c₂ }
+
+-- | An empty channel has no reads or writes and doesn't block execution.
+emptyChannel :: Channel
+emptyChannel = Channel Nothing Nothing Nothing Nothing
+
+-- | Write to the ports specified by the given memory address. This
+-- will clear all the channels not being written to (by setting them
+-- to Nothing).
+--
+-- The ports to use are specified by bits 5–8 of the address. These
+-- bits correspond respectively to up, left, down and right. Bits 5
+-- and 7 are inverted—0 turns the channel *on*.
+writePort :: F18Word    -- ^ The address to write to. Only bits 5–8 are considered.
+             -> F18Word -- ^ The word to write to the channel.
+             -> Channel -- ^ The resulting channel, with any unused ports empty.
+writePort ports word = Channel { right = [ word |     testBit ports 8 ]
+                               , down  = [ word | not $ testBit ports 7 ]
+                               , left  = [ word |     testBit ports 6 ]
+                               , up    = [ word | not $ testBit ports 5 ] }
+
+-- | Read the inputs from the ports specified by the given
+-- address. The address is handled the same way as in
+-- @'writePort'@. Returns @Nothing@ if blocked on the read.
+--
+-- If more than one of the read ports has data, this currently just
+-- chooses the first one based on the right, down, left, up order. I
+-- don't know if this is the correct behavior—perhaps I should just
+-- xor them together or something?
+readPort :: F18Word -> Channel -> Maybe F18Word
+readPort ports Channel {..} =  [ word |     testBit ports 8, word <- right ]
+                           <|> [ word | not $ testBit ports 7, word <- down  ]
+                           <|> [ word |     testBit ports 6, word <- left  ]
+                           <|> [ word | not $ testBit ports 5, word <- up    ]
--- src/Language/ArrayForth/State.hs
+++ src/Language/ArrayForth/State.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE NamedFieldPuns  #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns      #-}
+{-# LANGUAGE RecordWildCards     #-}
 -- | This module defines types and functions for working with the
 -- state of a single core.
--- 
+--
 -- The most important type is State, which contains all the
 -- information about the core. This includes the registers, the
 -- memory, both stacks and communication ports. Right now, it's just a
@@ -13,83 +13,38 @@
 -- memory of a chip and its communication channels.
 module Language.ArrayForth.State where
 
-import           Data.Bits                         ((.&.), shiftR, testBit)
 import           Data.Functor                      ((<$>))
 import           Data.Vector.Unboxed               (Vector, (//))
 import qualified Data.Vector.Unboxed               as V
 
 import           Text.Printf                       (printf)
 
+import           Language.ArrayForth.Channel
 import           Language.ArrayForth.NativeProgram
 import           Language.ArrayForth.Opcode        (F18Word)
 import           Language.ArrayForth.Stack
 
   -- TODO: Figure out how to deal with different reads in ports.
 
--- | The four communication ports each core can use. These are
--- mirrored, so "up" actually means "down" on half the cores!
--- 
--- The order is based on the way the channels are addressed, from
--- least signficant bit *up*. This is the reverse of how they're
--- presented in the manual: we do uldr where the manual has rdlu.
-data Port = U -- ^ up
-          | L -- ^ left 
-          | D -- ^ down
-          | R -- ^ right
-          deriving (Show, Eq, Bounded, Enum)
-
--- | An action on a communication port. 
-data Action = Read F18Word | Write F18Word deriving (Show, Eq)
-
--- | Either read from or write to the ports specified by the memory
--- address. The ports are specified by four bits in the address.
---
--- Each port is controlled by one bit in the order @R@, @D@, @L@,
--- @U@. @D@ and @U@ are inverted—they are used if their control bit is
--- set to @0@.
---
--- This means the pattern to use all the ports is 1010; consequently,
--- using no ports is 0101. However, setting 0101 is really meaningless
--- and will map to not listening on *any* port, which is actually
--- represented with @Channel Nothing@.
-setPort :: F18Word    -- ^ The address specifying which ports to use. Only
-                     -- bits 5–8 matter, the rest are ignored.
-           -> Action  -- ^ The action to perform on the port, either a
-                     -- read or a write.
-           -> Channel
-setPort ports action = go $ (ports `shiftR` 4) .&. 0xF
-  where go p | p == 0x5   = Channel Nothing
-             | otherwise = Channel $ Just (filter enabled [U, D, L, R], action)
-        enabled p = testBit ports $ fromEnum p + 5
-
--- | Read the input coming in on the given ports. 
-readPort :: Channel -> Maybe F18Word
-readPort (Channel word) = word >>= go
-  where go (_, Read x)  = Just x
-        go (_, Write _) = Nothing 
-
--- | A channel representing the four communication directions a core
--- may use. In practice, these will either be hooked up to other cores
--- or to IO. Nothing represents no message; if there is a word,
--- execution will block.
-newtype Channel = Channel (Maybe ([Port], Action)) deriving (Show, Eq)
-
--- | An empty channel has no reads or writes and doesn't block execution.
-emptyChannel :: Channel
-emptyChannel = Channel Nothing
-
 -- | The chip's RAM, ROM and IO channels. The RAM and ROM should each
 -- contain 64 words.
-data Memory = Memory { ram :: Vector Int
-                     , rom :: Vector Int
-                     , io  :: Channel } deriving (Show, Eq)
+--
+-- For now, input and output is split into two different types, even
+-- though they're combined on the physical chip. I'm simply not sure
+-- how to handle the case that both chips simultaneously write to the
+-- same channel.
+data Memory = Memory { ram    :: Vector Int
+                     , rom    :: Vector Int
+                     , input  :: Channel
+                     , output :: Channel } deriving (Show, Eq)
 
 -- | Memory with RAM and ROM zeroed out and nothing on the
--- communication channels. 
+-- communication channels.
 emptyMem :: Memory
-emptyMem = Memory { ram = V.replicate 64 0
-                  , rom = V.replicate 64 0
-                  , io  = emptyChannel }
+emptyMem = Memory { ram    = V.replicate 64 0
+                  , rom    = V.replicate 64 0
+                  , input  = emptyChannel
+                  , output = emptyChannel }
 
 -- | The number of words in memory. Both ram and rom are this
 -- size. For some reason, the ram and rom address spaces are *double*
@@ -101,11 +56,11 @@ memSize = 0x03F
 -- communication channels of a core. Note that all the fields are
 -- strict; they should also be unboxed thanks to
 -- @-funbox-strict-fields@ (set in the .cabal file).
--- 
+--
 -- For now, this is just a record; however, I might rewrite it to use
 -- lenses in the near future.
 data State =
-  State { a, b, p, r, s, t       :: !F18Word 
+  State { a, b, p, r, s, t       :: !F18Word
         , i                      :: !(Maybe F18Word)
           -- ^ the i register can be @Nothing@ if it is blocked on a
           -- communication port.
@@ -167,18 +122,18 @@ toMem = fromIntegral . (`mod` 64)
 (!) :: Memory -> F18Word -> Maybe F18Word
 Memory {..} ! i | i < 2 * memSize = Just . fromIntegral $ ram V.! toMem i
                 | i < 4 * memSize = Just . fromIntegral $ rom V.! toMem i
-                | otherwise       = readPort io
+                | otherwise       = readPort i input
 
--- TODO: Fix how this deals with blocking!
--- | Set the memory using Forth words. Returns @Nothing@ if blocked on a write.
-set :: State -> F18Word -> F18Word -> Maybe State
+-- | Set the memory using Forth words. A state with anything in the
+-- output channel remains blocked until one of the active ports is
+-- read.
+set :: State -> F18Word -> F18Word -> State
 set state@State {memory = memory@Memory {..}} i value
-  | i < 2 * memSize = Just updated
+  | i < 2 * memSize = state { memory = updatedRam }
   | i < 4 * memSize = error "Cannot set memory in the ROM!"
-  | otherwise       = if io == emptyChannel then Just updatedIO else Nothing
-  where updated = state {
-          memory = memory { ram = ram // [(toMem i, fromIntegral value)] }}
-        updatedIO = state { memory = memory { io = setPort i (Write value) } }
+  | otherwise       = state { memory = updatedOutput }
+  where updatedRam = memory { ram = ram // [(toMem i, fromIntegral value)] }
+        updatedOutput = memory { output = writePort i value }
 
 -- | Loads the given program into memory at the given starting
 -- position.
